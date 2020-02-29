aeb9422ece0dc67ba312c09212498d0865b5c3dc
Gus Eulalio <guga.emc@gmail.com>
2020-02-29 20:10:57 +1100

    Initial Commit

---
+++ Impromptu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Impromptu
+//
+//  Created by Gustavo E M Cabral on 29/2/20.
+//  Copyright © 2020 Gustavo Eulalio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
